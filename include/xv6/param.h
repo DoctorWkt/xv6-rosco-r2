9db72736421e49985d96ac9fed84dab075e39dfb
Warren Toomey <wkt@tuhs.org>
2024-09-17 10:37:09 +1000

    cmds/simple: Imported more commands from the MMU09 project.

--- include/xv6/param.h
+++ include/xv6/param.h
@@ -4,6 +4,6 @@
 #define MAXOPBLOCKS  10			// max # of blocks any FS op writes
 #define LOGSIZE      (MAXOPBLOCKS)	// max data blocks in on-disk log
 #define NBUF         (MAXOPBLOCKS)	// size of disk block cache
-#define FSSIZE       1000		// size of file system in blocks
+#define FSSIZE       3000		// size of file system in blocks
 
 #define MAXARGS	     100		// Maximum # command-line arguments
