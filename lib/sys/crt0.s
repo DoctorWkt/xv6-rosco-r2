6b939174b706fa71b49bf9a7ae8ad183f29aa018
Warren Toomey <wkt@tuhs.org>
2024-10-04 07:39:36 +1000

    lib/sys/crt0.s: Added an "a.out" header which we will use later when we have processes.

--- lib/sys/crt0.s
+++ lib/sys/crt0.s
@@ -1,13 +1,18 @@
 ; This is the xv6 userland initialization code.
-; A lot of it is borrowed from the rosco_m68k init.S file,
-; so it's under the rosco_m68k license.
+; Some of it is borrowed from the rosco_m68k init.S file.
 
 	section .init
 
 _start::                          ; entrypoint
+				  ; The a.ouh header looks like this
+    jmp      _start2		  ; uint16_t: magic number $600C
+    dc.w     $0000		  ; uint16_t: padding
+    dc.l     _code_end		  ; uint32_t: end of machine code
+    dc.l     _bss_end		  ; uint32_t: end of BSS
+    dc.l     $10000		  ; uint32_t: total size of process in bytes
+_start2:
     lea.l   __kinit,A0
     jsr     (A0)                  ; prepare C environment
-    ; bsr.s   CALL_CTORS            ; Call global constructors
     lea.l   main,A0
     jsr     (A0)                  ; Run the program
 
