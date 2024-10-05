7b1828ab1c93b938b6ce436c3e0fb4a258b1f5f9
Warren Toomey <wkt@tuhs.org>
2024-10-06 09:34:12 +1000

    kern/exec.c: I reworked this to ensure we can cleanly move to the new program's stack.

--- kern/exec.c
+++ kern/exec.c
@@ -8,19 +8,27 @@
 #include <errno.h>
 #include <a.out.h>
 
-// For some reason, this can't be on the stack. Why?
-static  char *lowptr;
+// We use variables that are not on the stack
+// to ensure that they are not tromped when
+// we move the arguments to the top of stack
+static int exec_i;
+static int exec_len;
+static int nframes, basereg;
+static int exec_cnt;
+static int exec_fd;
+static int exec_argc;
+static char *oldsbrk;
+static char *kern_argv[MAXARGS];	// Copy in kernel memory
+static char *exec_argv[MAXARGS];	// Copy in new program memory
+static char *exec_ptr;
+static char *exec_lowptr;
+static char **exec_cptr;
 
-// Exec argv[0] with the given arguments.
+
+// Spawn argv[0] with the given arguments.
 // Return if this fails.
 void sys_exec(char *pathname, char *argv[]) {
-  int i, len, cnt, fd, argc;
-  int nframes, basereg;
-  char *oldsbrk;
-  char *ptr;
-  char **cptr;
-  char *kern_argv[MAXARGS];	// Copy in kernel memory
-  char *exec_argv[MAXARGS];	// Copy in new program
+  int argc;
   struct aout ahdr;		// a.out header
 
   // Set no errors yet
@@ -29,24 +37,27 @@ void sys_exec(char *pathname, char *argv[]) {
   // Count the number of arguments
   for (argc=0; argv[argc]!=NULL; argc++) ;
 
+  // Save the argc off the stack
+  exec_argc= argc;
+
   // If there are no arguments, or too many, fail.
-  if (argc < 1 || argc > MAXARGS) {
+  if (exec_argc < 1 || exec_argc > MAXARGS) {
     set_errno(EFAULT); return;
   }
 
   // Check that all the argv's are valid
-  for (i=0; i < argc; i++)
-    if (argv[i]==NULL) {
+  for (exec_i=0; exec_i < exec_argc; exec_i++)
+    if (argv[exec_i]==NULL) {
       set_errno(EFAULT); return;
     }
 
   // Try to open the program
-  fd= sys_open(argv[0], O_RDONLY);
-  if (fd== -1) { set_errno(ENOENT); return; }
+  exec_fd= sys_open(pathname, O_RDONLY);
+  if (exec_fd== -1) { set_errno(ENOENT); return; }
 
   // Ensure the file has an a.out header
-  cnt= sys_read(fd, (char *)&ahdr, sizeof(struct aout));
-  if (cnt!= sizeof(struct aout)) {
+  exec_cnt= sys_read(exec_fd, (char *)&ahdr, sizeof(struct aout));
+  if (exec_cnt!= sizeof(struct aout)) {
     set_errno(EINVAL); return;
   }
 
@@ -73,41 +84,44 @@ void sys_exec(char *pathname, char *argv[]) {
 
   // Count the size of all the argument strings,
   // including the terminating NUL.
-  for (cnt=0, i=0; i< argc; i++)
-    cnt += strlen(argv[i])+1;
+  for (exec_cnt=0, exec_i=0; exec_i< exec_argc; exec_i++)
+    exec_cnt += strlen(argv[exec_i])+1;
 
-  // Get a pointer to cnt amount of free kernel memory.
-  oldsbrk= ptr= (char *)ksbrk(cnt);
+  // Get a pointer to exec_cnt amount of free kernel memory.
+  oldsbrk= exec_ptr= (char *)ksbrk(exec_cnt);
   if (oldsbrk== (void *) -1)
     panic("exec");
 
   // Copy the arguments down to kernel memory
-  for (i=0; i< argc; i++) {
-    len= strlen(argv[i])+1;
-    memmove(ptr, argv[i], len);
-    kern_argv[i]= ptr;
-    ptr+= len;
+  for (exec_i=0; exec_i< exec_argc; exec_i++) {
+    exec_len= strlen(argv[exec_i])+1;
+    memmove(exec_ptr, argv[exec_i], exec_len);
+    kern_argv[exec_i]= exec_ptr;
+    exec_ptr+= exec_len;
   }
 
-  // With the arguments safe, switch over to the
-  // new program's memory
+  // Determine the top of the new program's stack.
+  // Ensure that it is word aligned.
+  ahdr.a_total_size &= 0xfffffffe;
+  exec_ptr= (char *)START_ADDR + ahdr.a_total_size;
+  // cprintf("Will set SP to 0x%p for %s\n", exec_ptr, pathname);
+
+  // With the arguments safe, switch
+  // over to the new program's memory.
+  // Also set the stack pointer to the top of
+  // kernel memory, so that future interrupts
+  // won't touch the new program's stack
+  __asm__ ("    move.l #0x100000,%sp");
   setbasereg(basereg);
 
-  // Set the top of the program's stack.
-  // Ensure that it is word aligned
-  ahdr.a_total_size &= 0xfffffffe;
-  ptr= (char *)START_ADDR + ahdr.a_total_size;
-  // ptr= (char *)START_ADDR + 1024 * 1024;
-  // cprintf("Set SP to 0x%p\n", ptr);
-
-  // Copy each argument on to the new program's
-  // stack and save the pointer value
-  for (i= argc-1; i>=0; i--) {
+  // Copy each argument on to the stack
+  // and save the pointer value
+  for (exec_i= exec_argc-1; exec_i>=0; exec_i--) {
     // Count the strings's length and the NUL.
-    cnt= strlen(kern_argv[i])+1;
-    ptr -= cnt;
-    memmove(ptr, kern_argv[i], cnt);
-    exec_argv[i]= ptr;
+    exec_cnt= strlen(kern_argv[exec_i])+1;
+    exec_ptr -= exec_cnt;
+    memmove(exec_ptr, kern_argv[exec_i], exec_cnt);
+    exec_argv[exec_i]= exec_ptr;
   }
 
   // Move the pointer down to make room
@@ -118,29 +132,29 @@ void sys_exec(char *pathname, char *argv[]) {
   // to point to the argv array.
   // Align the pointer on a word boundary.
   // Save a copy for later.
-  ptr -= sizeof(int) + argc * sizeof(char *);
-  ptr -= 2 * sizeof(char *);
-  ptr = (char *)((int)ptr & 0xfffffffe);
-  lowptr= ptr;
+  exec_ptr -= sizeof(int) + exec_argc * sizeof(char *);
+  exec_ptr -= 2 * sizeof(char *);
+  exec_ptr = (char *)((int)exec_ptr & 0xfffffffe);
+  exec_lowptr= exec_ptr;
 
   // Copy argc to the stack
-  memmove(ptr, (void *)&argc, sizeof(int));
-  ptr += sizeof(int);
+  memmove(exec_ptr, (void *)&exec_argc, sizeof(int));
+  exec_ptr += sizeof(int);
 
   // Put in the argv pointer after argc
-  cptr= (char **)ptr;
-  ptr += sizeof(char *);
-  *cptr= ptr;
+  exec_cptr= (char **)exec_ptr;
+  exec_ptr += sizeof(char *);
+  *exec_cptr= exec_ptr;
 
   // Put the argument pointers on the stack
-  for (i=0; i < argc; i++) {
-    memmove(ptr, &(exec_argv[i]), sizeof(char *));
-    ptr += sizeof(char *);
+  for (exec_i=0; exec_i < exec_argc; exec_i++) {
+    memmove(exec_ptr, &(exec_argv[exec_i]), sizeof(char *));
+    exec_ptr += sizeof(char *);
   }
 
   // Finish with the NULL
-  cptr= (char **)ptr;
-  *cptr= NULL;
+  exec_cptr= (char **)exec_ptr;
+  *exec_cptr= NULL;
 
   // Return the allocated kernel memory
   kbrk(oldsbrk);
@@ -148,21 +162,21 @@ void sys_exec(char *pathname, char *argv[]) {
   // Load the program into userspace.
   // Do this after we set up the arguments
   // in case we might tromp on them.
-  ptr= (char *)START_ADDR;
-  sys_lseek(fd, 0, 0);
+  exec_ptr= (char *)START_ADDR;
+  sys_lseek(exec_fd, 0, 0);
   while (1) {
-    cnt= sys_read(fd, ptr, 512);
-    if (cnt<=0) break;
-    ptr+= cnt;
+    exec_cnt= sys_read(exec_fd, exec_ptr, 512);
+    if (exec_cnt<=0) break;
+    exec_ptr+= exec_cnt;
   }
-  sys_close(fd);
+  sys_close(exec_fd);
 
   // Set the stack pointer to point at the argc on the stack
   // and jump to the first instruction in the program
   __asm__ (
 	"    move.l %[temp],%%sp\n"
 	"    jmp 0x100000\n"			// XXX Same as START_ADDR
-	: [temp] "+d"(lowptr));
+	: [temp] "+d"(exec_lowptr));
 }
 
 // When a program exits, we reexec the shell
